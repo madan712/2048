35f11eea2df7efa3ab8f90f7556dd22cbb5794ab
Madan Chaudhary <madan712@gmail.com>
2022-01-31 21:07:47 +0000

    Updating app

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import _ from 'lodash';
 import GestureRecognizer from 'react-native-swipe-gestures';
 
 export default function App() {
+
+	const size = 4;
+
+	var height = Dimensions.get('window').height;
+	var width = Dimensions.get('window').width;
 
 	const onSwipeUp = (state) => {
 		console.log('onSwipeUp');
@@ -29,8 +34,31 @@ export default function App() {
 			onSwipeLeft={(state) => onSwipeLeft(state)}
 			onSwipeRight={(state) => onSwipeRight(state)}
 		>
-			<Text>Open up App.tsx to start working on your app!</Text>
-			<StatusBar style="auto" />
+
+			<View style={{ backgroundColor: '#BDBDBD', flex: 1, width: '100%',  justifyContent: 'center' }}>
+
+				<View style={{   flexDirection: 'column' }}>
+					{
+						_.times(size, (r) => {
+							return <View key={r} style={{ flexDirection: 'row', justifyContent: 'center'}}>
+								{
+									_.times(size, (c) => {
+
+
+										return <View style={{ margin: 2, padding: 20, backgroundColor: '#FFFFFF', borderWidth: 1 }} key={r + c} ><Text style={{fontSize: 20, fontWeight: "bold"}}>10</Text></View>
+
+
+									})
+								}
+							</View>
+						})
+					}
+
+				</View>
+
+			</View>
+
+
 		</GestureRecognizer>
 	);
 }
